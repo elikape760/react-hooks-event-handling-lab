d5e6b5007bc74f780de049b5133c4d59b50325ef
Elias <elikapeluschnik@gmail.com>
2021-11-04 12:01:51 -0400

    commit tree 2a3f8f575fb57da848ab66655dd0ac8b3256ac2d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    function focusHandler(e) {
+        // console.log(`${e.target.name}: ${e.target.value}`);
+        console.log('Good!')
+    }
+    return (
+        <>
+            <button onFocus={focusHandler} onBlur={(e) => { console.log('Hey! Eyes on me!'); }}>Eyes on me</button>
+            {/* <button onBlur={(e) => { console.log('Hey! Eyes on me!'); }}>Lose Focus</button> */}
+        </>
+    )
+}
+
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+    function changeHandler(e) {
+        e.preventDefault()
+        console.log(`${e.target.name}: ${e.target.value}`);
+    }
+
+    return (
+        <form >
+            <input onChange={changeHandler} type="password" name="password" placeholder="Entering Password..." value="" />
+        </form>
+    )
+}
+
+export default Keypad;
